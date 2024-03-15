5c2e61a15325c5f329f197d89967f7adbdc69350
joelshajan1551 <joelshajan0000@gmail.com>
2024-03-15 21:50:54 +0530

    login

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ import { Schema, model, models } from "mongoose";
 const UserSchemaa = newSchema({
   email: {
     type: String,
-    unique: [true, "Email aldready exist"],
+    unique: [true, "Email already exist"],
     required: [true, "Email is required"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    match: [],
   },
 });
